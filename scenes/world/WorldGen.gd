8ab72f1d8912cfa7e0ee42e06102ac1d4637bdd2
Aaron7317 <aaron.boyd.org@gmail.com>
2023-03-08 12:34:18 -0700

    Weird terrain generation fixed and caves work

--- scenes/world/WorldGen.gd
+++ scenes/world/WorldGen.gd
@@ -6,9 +6,9 @@ var noise : FastNoiseLite = FastNoiseLite.new()
 var chunks = {}
 
 var standard_layer = 0
-var world_length = 32 * chunk_size
-var world_depth = 4 * chunk_size
-var world_height = 8 * chunk_size
+var world_length = 64 * chunk_size
+var world_depth = 16 * chunk_size
+var world_height = 16 * chunk_size
 
 
 
@@ -17,25 +17,28 @@ var world_height = 8 * chunk_size
 func _ready():
 	randomize()
 	
-	noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
+	noise.noise_type = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
 	noise.seed = randi()
-	noise.frequency = 0.001 # Values closer to zero result in smoother terrain
+	noise.frequency = 0.0015 # Values closer to zero result in smoother terrain
 	
 	
 	for row in range(-world_depth / chunk_size, world_height / chunk_size):
 		for col in range(0, world_length / chunk_size):
 			chunks[Vector2i(col * chunk_size, row * chunk_size)] = _get_empty_chunk()
-			
-
+	
 	
 	$TileMap.clear()
-	#This still draws weird but only sometimes
 	for x in range(0, world_length):
 		var y_max = ceil(noise.get_noise_1d(x) * world_height)
 		
 		for y in range(y_max, world_depth):
 			chunks[_get_nearest_chunk(Vector2i(x, y))][y % chunk_size][x % chunk_size] = 0
-	
+		
+		for y in range (y_max, world_depth):
+			var yy = noise.get_noise_2d(x, y)
+			if abs(yy) < 0.04:
+				chunks[_get_nearest_chunk(Vector2i(x, y))][y % chunk_size][x % chunk_size] = -1
+		
 	# eventually have chunks drawn dynamically
 	for i in chunks:
 		$TileMap.set_cells_terrain_connect(0, _search_chunk_for_cell_type(i, 0), 0, 0)
@@ -47,7 +50,17 @@ func _process(delta):
 	pass
 
 func _get_nearest_chunk(point : Vector2i) -> Vector2i:
-	return Vector2i(point.x - point.x % chunk_size, point.y - point.y % chunk_size)
+	var nearest_chunk = Vector2i(0, 0)
+	if (point.x < 0):
+		nearest_chunk.x = point.x - (point.x % chunk_size) - chunk_size
+	else:
+		nearest_chunk.x = point.x - point.x % chunk_size
+	if (point.y < 0):
+		# I Could not tell you why the +1 is necessary but it fixes wierd generation
+		nearest_chunk.y = (point.y + 1) - ((point.y + 1) % chunk_size) - chunk_size
+	else:
+		nearest_chunk.y = point.y - point.y % chunk_size
+	return nearest_chunk
 
 func _get_cells_from_chunk(chunk_pos : Vector2i):
 	var cells = []
